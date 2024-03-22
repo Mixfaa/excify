f444ea1839e2c6f21de0e06271d6575b50d78cac
Миша Език <mishunder9354@gmail.com>
2024-03-23 00:01:42 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19"
 }
 
 group = "com.mixfa"
@@ -10,7 +11,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    implementation("io.arrow-kt:arrow-core:1.2.1")
+
+    implementation("com.squareup:kotlinpoet:1.16.0")
+    implementation("com.squareup:kotlinpoet-ksp:1.16.0")
+
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.23-1.0.19")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
 
 tasks.test {
